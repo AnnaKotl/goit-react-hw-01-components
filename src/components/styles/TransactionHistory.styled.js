19d4aeaae6fff5609858ae6c548ffb5dc2292272
Анна Котляр <kotlyaranya1771@gmail.com>
2023-08-06 14:39:51 +0300

    text-transform: capitalize;

--- src/components/styles/TransactionHistory.styled.js
+++ src/components/styles/TransactionHistory.styled.js
@@ -28,5 +28,6 @@ export const TableTh = styled.th`
   font-size: 20px;
   text-align: center;
   border: 2px solid #e0e0e0;
+  text-transform: uppercase;
 `;
 
--- src/components/styles/TransactionItem.styled.js
+++ src/components/styles/TransactionItem.styled.js
@@ -4,7 +4,8 @@ export const TableTd = styled.td`
   text-align: center;
   font-size: 16px;
   min-width: 160px;
-    padding: 10px 20px;
+  padding: 10px 20px;
   text-align: center;
   border: 2px solid #e0e0e0;
+  text-transform: capitalize;
 `;
